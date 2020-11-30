2bb337e03107a700a9e925d622d5b3ce0b90275e
Max Kopjev <GandifilMK@gmail.com>
2020-11-30 20:23:57 +0300

    Change iteration.

--- shedule_01iter.py
+++ shedule_01iter.py
@@ -1,35 +1,59 @@
+import signal
+import sys
+from math import factorial
+
 import sheduling
 from itertools import *
 
 cellsCount = 6 * 2 * len(sheduling.times)
-lessons1 = [None for _ in range(cellsCount - len(sheduling.lessons1))] + sheduling.lessons1
-lessons2 = [None for _ in range(cellsCount - len(sheduling.lessons2))] + sheduling.lessons2
+
+
+def convert(lessonsPrint, indexes):
+    assert len(lessonsPrint) == len(indexes)
+    lessons = [None for _ in range(cellsCount)]
+    for i in range(len(lessonsPrint)):
+        lessons[indexes[i]] = lessonsPrint[i]
+    shedule = sheduling.Shedule()
+    shedule.fillConcreteWithRandom(lessons)
+    return shedule
+
 
 maxFitness = -1000000000
 theBestShedule = None
 
 i = 0
-for lessonIndexes1 in permutations(lessons1):
-    for lessonIndexes2 in permutations(lessons2):
-        print(lessonIndexes1)
-        print(lessonIndexes2)
-        print("Iter ", i)
-        i += 1
 
-        shedule = sheduling.GlobalShedule()
-        shedule.for247 = sheduling.Shedule()
-        shedule.for247.fillConcreteWithRandom(lessonIndexes2)
-        shedule.for248 = sheduling.Shedule()
-        shedule.for248.fillConcreteWithRandom(lessonIndexes1)
+
+def permCount(n, m):
+    return factorial(n) / factorial(n - m)
+
+
+def save():
+    with open('result.txt', 'w') as f:
+        print(theBestShedule, file=f)
+        print(theBestShedule.fitness(), file=f)
+
+
+shedule = sheduling.GlobalShedule()
+length1 = permCount(cellsCount, len(sheduling.lessons1))
+length2 = permCount(cellsCount, len(sheduling.lessons2))
+length = int(length1 * length2)
+
+for lessonIndexes1 in permutations(range(cellsCount), len(sheduling.lessons1)):
+    for lessonIndexes2 in permutations(range(cellsCount - 1, -1, -1), len(sheduling.lessons2)):
+        # print("Iter ", i)
+        i += 1
+        shedule.for248 = convert(sheduling.lessons1, lessonIndexes1)
+        shedule.for247 = convert(sheduling.lessons2, lessonIndexes2)
 
         if shedule.isValid():
-            print("Check ", i)
             fitness = shedule.fitness()
+            print("Check ", i, "/", length, " shedule with fitness ", fitness)
             if fitness > maxFitness:
-                print(shedule)
-                print(fitness)
+                print("New best shedule!")
                 theBestShedule = shedule
                 maxFitness = fitness
+                save()
 
 print(theBestShedule)
 print(theBestShedule.fitness())
